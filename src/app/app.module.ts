3feefad00f8252f7364fe9bb7eaaa8ff3093ec01
gitgregoryfuntera <gregory.funtera@gmail.com>
2024-02-10 20:32:32 +0800

    [sc-17]: removed unneeded module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { ComponentsModule } from './components/components.module';
 
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
-import { AccordionModule } from 'ngx-bootstrap/accordion';
+
 @NgModule({
     declarations: [
         AppComponent,
@@ -37,7 +37,6 @@ import { AccordionModule } from 'ngx-bootstrap/accordion';
             positionClass: 'toast-top-right',
             preventDuplicates: true
         }),
-        AccordionModule.forRoot()
     ],
     providers: [
         {
