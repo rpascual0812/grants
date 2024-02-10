63aadb4d2b7f99b73bd31b0bc2e9f0ef13cee571
gitgregoryfuntera <gregory.funtera@gmail.com>
2024-02-11 06:24:03 +0800

    [sc-17]: fixed merge conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,6 @@ import { ComponentsModule } from './components/components.module';
 
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
-import { AccordionModule } from 'ngx-bootstrap/accordion';
 import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component';
 @NgModule({
     declarations: [
