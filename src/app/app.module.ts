579196f5e4cd4178ff38caf4b0146a500e004a3e
gitgregoryfuntera <gregory.funtera@gmail.com>
2024-02-11 06:25:50 +0800

    [sc-17]: fix merge conflicts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { ComponentsModule } from './components/components.module';
 
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 import { MainLayoutComponent } from './layouts/main-layout/main-layout.component';
+import { PublicLayoutComponent } from './layouts/public-layout/public-layout.component';
 @NgModule({
     declarations: [
         AppComponent,
