6a437099d2cc880e7d983795668d227f60f4cfc8
Rafael Pascual <rpascual0812@gmail.com>
2024-02-17 21:14:29 +0800

    Added application CRUD

--- src/app/layouts/public-layout/public-layout.module.ts
+++ src/app/layouts/public-layout/public-layout.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { PublicLayoutRoutes } from './public-layout.routing';
 import { ApplicationStatusModule } from 'src/app/pages/applications/application-status/application-status.module';
 import { ApplicationAuthModule } from 'src/app/pages/applications/application-auth/application-auth.module';
-import { ApplicationNewComponent } from 'src/app/pages/applications/application-new/application-new.component';
 import { ApplicationNewModule } from 'src/app/pages/applications/application-new/application-new.module';
 
 @NgModule({
--- src/app/pages/applications/application-new/modules/proponent-information/proponent-information.module.ts
+++ src/app/pages/applications/application-new/modules/proponent-information/proponent-information.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProponentInformationComponent } from './proponent-information.component';
 
 @NgModule({
     declarations: [ProponentInformationComponent],
     exports: [ProponentInformationComponent],
-    imports: [CommonModule],
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+    ],
 })
-export class ProponentInformationModule {}
+export class ProponentInformationModule { }
