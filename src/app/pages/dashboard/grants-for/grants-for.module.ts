ee516d3008c8aeb1825f613b513be42cb1b7e704
Rafael Pascual <rpascual0812@gmail.com>
2024-06-23 10:08:35 +0800

    Grant Types colors

--- src/app/pages/dashboard/grants-for/grants-for.module.ts
+++ src/app/pages/dashboard/grants-for/grants-for.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GrantsForComponent } from './grants-for.component';
 import { BaseChartDirective } from 'ng2-charts';
-
+import { NgbPopoverModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
     declarations: [
@@ -10,6 +10,7 @@ import { BaseChartDirective } from 'ng2-charts';
     ],
     imports: [
         CommonModule,
+        NgbPopoverModule,
         BaseChartDirective
     ],
     exports: [GrantsForComponent]
