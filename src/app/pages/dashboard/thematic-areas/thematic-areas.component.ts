902e275b482b95e54ae0a0ca012aaddd0f6ab70e
gitgregoryfuntera <gregory.funtera@gmail.com>
2024-09-02 20:37:01 +0800

    [sc-883]: Dashboard - Samdhana Thematic Areas

--- src/app/pages/dashboard/thematic-areas/thematic-areas.component.ts
+++ src/app/pages/dashboard/thematic-areas/thematic-areas.component.ts
@@ -1,78 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-thematic-areas',
     templateUrl: './thematic-areas.component.html',
-    styleUrls: ['./thematic-areas.component.scss']
+    styleUrls: ['./thematic-areas.component.scss'],
 })
-export class ThematicAreasComponent {
-    areas: any = [
+export class ThematicAreasComponent implements OnInit {
+    areas = [
         {
             title: 'Tenure',
-            descriptions: [
-                '(sub threes) IP territories/ domains;',
-                'agrarian rights',
-                'marine tenure rights',
-                'tenure conflict resolution'
-            ],
-            color: '#83380c'
+            descriptions: `Securing tenurial rights to territories and places underpins cultural and social security, a necessary condition for sustainable development of IPLCs across Asia`,
+            color: '#83380c',
+            displayShowMore: false,
         },
         {
-            title: 'Governance and Leadership Development',
-            descriptions: [
-                '(sub themes) Indigenous governance, including the roles of women, youth and differently abled people;',
-                'Instutional strengthening of community or local institutions',
-                'CSO/ NGO organizationl development',
-                'IPLC rights awareness and leadership capacity development',
-                'CSO/NGO/ IPLC dfending and expanding civic space'
-            ],
-            color: '#d27b26'
+            title: 'Indigenous  Governance and Leadership Development',
+            descriptions: `Through a combination of peer learning, systems thinking, reflection, and accompaniment, Samdhana supports communities’ right to self-determination. We support strengthening of governance and increasing capacities for leadership to help community leaders, NGOs, and other civil society leaders to reflect upon their experiences and strengthen their skills while also elevating the roles and responsibilities of local leaders. Grassroots organizations and local CSOs are supported to carry out their roles for the social and environmental movement.`,
+            color: '#d27b26',
+            displayShowMore: false,
         },
         {
             title: 'Natural Resource Governance',
-            descriptions: [
-                '(sub themes) IPLC rights to access and control',
-                'Community-led natural resource management and development',
-                'Natural resource conflict resolution;',
-                'Environmental protection, conservation and restoration'
-            ],
-            color: '#9c4715'
+            descriptions: `Integrating indigenous rights and interests into policies and programs for land and resource management; supporting IPLCs to actively plan, develop, manage and care for their natural assets to fulfill their basic needs in a way that maintains the balanced relationship between people and ecosystems.`,
+            color: '#9c4715',
+            displayShowMore: false,
         },
         {
             title: 'Next Generation',
-            descriptions: [
-                '(sub themes) YOUTH leadership and capacity development',
-                'Education support',
-                'Inter-generational learning'
-            ],
-            color: '#b78121'
+            descriptions: `Focusing on youth leadership and capacity development; support for youth-led initiatives such as rights-based organizing, environmental protection, rights awareness; Supporting and creating opportunities for education and development of young people; and fostering inter-generational learning.`,
+            color: '#b78121',
+            displayShowMore: false,
         },
         {
             title: 'Living Economy',
-            descriptions: [
-                '(sub themes) Food security and sovereignty',
-                'Sustainable/ rural livelihoods',
-                'Social enterprise and income generation'
-            ],
-            color: '#c55d1e'
+            descriptions: `Support the development of sustainable and climate-resilient livelihoods and other resource management activities of rural communities that ensures food security and sovereignty; Support for the development of social enterprises owned and managed by IPLCs.`,
+            color: '#c55d1e',
+            displayShowMore: false,
         },
         {
-            title: 'Disability rights in the intersection of Environmental Justice',
-            descriptions: [
-                '(sub themes) Diffables capacity development',
-                'Support to diffables for livelihoods and increased participation'
-            ],
-            color: '#d29427'
+            title: 'Gender Equality, Disability (Rights) and Social Inclusion (GEDSI)',
+            descriptions: `Cross cutting theme across Samdhana's work, to elevate and increase recognition of the role of women in indigenous communities, highlighting the vital contributions they make to sustainable livelihoods, culture, and community education. We support local women in taking on leadership roles while also expanding social inclusion to involve differently abled people, youth groups, and the elderly.`,
+            color: '#d29427',
+            displayShowMore: false,
         },
         {
             title: 'Mitigating & Adapting to Climate Change',
-            descriptions: [
-                '(sub themes) Safeguarding tenure rights to landscapes earmarked for low emissions development',
-                'Loss and damage Ecosystems-based adaptation (EbA)',
-                'Policy development, lobby and advocacy',
-                'Climate financing'
-            ],
-            color: '#d46623'
+            descriptions: `Helping secure land rights for IPLCs esp in landscapes earmarked for low carbon emissions. This involves outreach activities to communicate and clarify rights holders’ understanding of climate change and their roles as guardians of the landscape. Through carbon sequestration initiatives, rights mapping, green development planning, and community REDD+ projects, IPLCs are supported to lead in climate mitigation. Supporting the revival and promotion of indigenous knowledge and local practices as an untapped resource to mitigate and adapt to climate change; and supporting spaces for better understanding of IPLCs capacities for resilience building, in the face of loss and damage.`,
+            color: '#d46623',
+            displayShowMore: false,
         },
     ];
+
+    availableShowMoreItems: Record<string, string | boolean>[] = [];
+
+    ngOnInit() {
+        this.areas = this.areas.map((item) => {
+            if (item.descriptions.length > 200) {
+                item.displayShowMore = true;
+
+                this.availableShowMoreItems.push({
+                    title: item.title,
+                    isShowingMore: true,
+                });
+            }
+            return item;
+        });
+    }
+
+    getShowMoreFlag(title: string) {
+        const item = this.availableShowMoreItems.find((item) => item['title'] === title);
+        return item?.['isShowingMore'] || false;
+    }
+
+    handleOnToggleShowMore(title: string) {
+        this.availableShowMoreItems = this.availableShowMoreItems.map((item) => {
+            if (item['title'] === title) {
+                item['isShowingMore'] = !item['isShowingMore'];
+            }
+            return item;
+        });
+    }
 }
